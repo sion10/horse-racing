42abc63fb0ba529817120bc33fbe89565310dcb3
Jason R. Carrete <jasoncarrete5@gmail.com>
2017-07-03 23:40:51 -0400

    Corrected link href paths

--- php/login.php
+++ php/login.php
@@ -5,8 +5,8 @@
 		<meta author="Jason R. Carrete">
 		<meta charset="utf-8">
 		<meta name="viewport" content="width=device-width, initial-scale=1">
-		<link rel="icon" href="img/favicon.png">
-		<link rel="stylesheet" href="css/main.css">
+		<link rel="icon" href="../img/favicon.png">
+		<link rel="stylesheet" href="../css/main.css">
 	</head>
 	<body>
 		
