5d83d4349ac612ab8bc50ef02da3fad19a0be315
Jason R. Carrete <jasoncarrete5@gmail.com>
2017-07-03 23:39:48 -0400

    Linked favicon and stylesheet to login.php

--- php/login.php
+++ php/login.php
@@ -5,6 +5,8 @@
 		<meta author="Jason R. Carrete">
 		<meta charset="utf-8">
 		<meta name="viewport" content="width=device-width, initial-scale=1">
+		<link rel="icon" href="img/favicon.png">
+		<link rel="stylesheet" href="css/main.css">
 	</head>
 	<body>
 		
